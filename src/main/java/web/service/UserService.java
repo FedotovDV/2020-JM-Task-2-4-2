b34fcb60d03bcdb192546dd9d83c472d9f37c429
Dmitriy <fed3375546@yandex.ru>
2020-08-05 17:47:15 +0300

    UserService and RootConfig

---
+++ src/main/java/web/service/UserService.java
@@ -0,0 +1,20 @@
+package web.service;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> getUsers();
+
+    void addUser(User user);
+
+    void deleteUser(Long id);
+
+    void updateUser(User user);
+
+    User getUserById(Long id);
+
+    User getUserByName(String username);
+}
